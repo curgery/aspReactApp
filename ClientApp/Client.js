149bf2c57db730d6985ca24a8ccdc513a6e5eb92
curgery <androidgeek54@gmail.com>
2019-02-20 10:43:33 -0500

    Commit completing successful completion of redux devtools...

--- ClientApp/Client.js
+++ ClientApp/Client.js
@@ -3,12 +3,26 @@ import ReactDOM from "react-dom"
 import { browserHistory } from 'react-router';
 import { BrowserRouter as Router } from 'react-router-dom';
 
+import { Provider } from 'react-redux';
 import FullPage from "./Components/common/FullPage";
+import configureStore from "../redux/configureStore";
 
+const store = configureStore(window.__STATE__);
 
 ReactDOM.render(
-    <Router history={browserHistory}>
-        <FullPage/>
-    </Router>,
+    <Provider store={store}>
+        <Router history={browserHistory}>
+            <FullPage />
+        </Router>
+    </Provider>,
     document.getElementById("app")
 );
+
+
+
+// ReactDOM.render(
+//     <Router history={browserHistory}>
+//         <FullPage/>
+//     </Router>,
+//     document.getElementById("app")
+// );
--- ClientApp/Components/speakers/Speakers.js
+++ ClientApp/Components/speakers/Speakers.js
@@ -3,10 +3,10 @@ import PropTypes from 'prop-types';
 
 import SpeakersHeader from './SpeakersHeader';
 import SpeakerList from './SpeakerList';
-import axios from 'axios';
+// import axios from 'axios';
 
-// import { connect } from 'react-redux';
-// import { speakersFetchData } from ".././../../redux/actions/speakers";
+import { connect } from 'react-redux';
+import { speakersFetchData } from ".././../../redux/actions/speakers";
 
 class Speakers extends Component {
 
@@ -22,74 +22,75 @@ class Speakers extends Component {
 
     componentDidMount() {
 
-        // this.props.speakersFetchData();
+        this.props.speakersFetchData();
 
-        axios.get('/data/speakers.json')
-            .then((result)=> {
-                this.setState({
-                    appData: result.data,
-                    isLoading: false
-                });
-            })
-            .catch(error => {
-                if (error.response) {
-                    console.log(error.responderEnd);
-                }
-            });
+        // axios.get('/data/speakers.json')
+        //     .then((result)=> {
+        //         this.setState({
+        //             appData: result.data,
+        //             isLoading: false
+        //         });
+        //     })
+        //     .catch(error => {
+        //         if (error.response) {
+        //             console.log(error.responderEnd);
+        //         }
+        //     });
     }
 
 
     render() {
 
-        // if (this.props.isLoading) {
-        //     return <span><i>Loading...</i></span>
-        // }
-        // else if (this.props.hasErrored) {
-        //     return <span><b>Failed to load data: {this.props.errorMessage}</b></span>
-        // }
-        // else {
-        //     return (
-        //         <div>
-        //             {/* <SpeakersHeader/> */}
-        //             {/* <SpeakerList speakers={this.props.speakers} /> */}
-        //         </div>
-        //     );
-        // }
-
-        // if (this.state.isLoading) {
-        //     return <span><i>Loading...</i></span>
-        // }
-        // else {
+        if (this.props.isLoading) {
+            return <span><i>Loading...</i></span>
+        }
+        else if (this.props.hasErrored) {
+            return <span><b>Failed to load data: {this.props.errorMessage}</b></span>
+        }
+        else {
             return (
                 <div>
-                    {/* <SpeakersHeader/> */}
-                    <SpeakerList speakers={this.state.appData} />
-                    {/* <span>{JSON.stringify(this.state.appData)}</span> */}
+                    <SpeakersHeader/>
+                    <SpeakerList speakers={this.props.speakers} />
                 </div>
             );
         }
     }
+}
+
+                            // if (this.state.isLoading) {
+                            //     return <span><i>Loading...</i></span>
+                            // }
+                            // else {
+                                {/* return (
+                                    <div> */}
+                                        {/* <SpeakersHeader/> */}
+                                        {/* <SpeakerList speakers={this.state.appData} /> */}
+                                        {/* <span>{JSON.stringify(this.state.appData)}</span> */}
+                                    {/* </div> */}
+                                // );
+  
 
 
 Speakers.propTypes = {};
 Speakers.defaultProps = {};
 
 
-// const mapStateToProps = (state) => {
+const mapStateToProps = (state) => {
 
-//     return {
-//         speakers: state.speakers.data,
-//         hasErrored: state.speakers.hasErrored,
-//         isLoading: state.speakers.isLoading,
-//         errorMessage: state.speakers.errorMessage
-//     };
-// };
+    return {
+        speakers: state.speakers.data,
+        hasErrored: state.speakers.hasErrored,
+        isLoading: state.speakers.isLoading,
+        errorMessage: state.speakers.errorMessage
+    };
+};
 
 
-export default Speakers;
+// export default Speakers;
 
-// export default connect(mapStateToProps,
-//     { speakersFetchData })(Speakers)
+export default connect(mapStateToProps,
+    { speakersFetchData })(Speakers)
 
 
 // import React from 'react';
