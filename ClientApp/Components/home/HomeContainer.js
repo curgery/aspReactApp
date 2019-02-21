ac24d89eceab13d1a1aca107ea5da1ee372edd1b
curgery <androidgeek54@gmail.com>
2019-02-21 15:31:40 -0500

    Provided the 'session' components while providing update capability

--- ClientApp/Components/home/HomeContainer.js
+++ ClientApp/Components/home/HomeContainer.js
@@ -1,15 +1,27 @@
-
 import React from 'react';
-import HomeSpeakersCarousel from './HomeSpeakersCarousel';
+import HomeSessionCount from './HomeSessionCount';
+import HomeSessionFilter from './HomeSessionFilter';
+import HomeFilterView from './HomeFilterView';
+import HomeSessions from './HomeSessions';
 
-export default function HomeContainer() {
-  return (
-    <div className="container-main events">
-      <div className="row">
-        <div className="col-12">
-          <HomeSpeakersCarousel />
-        </div>
+export default function HomeContainer(props) { 
+  if (!props.isLoading) {
+    return (
+      <div className="container-main events">
+          <div className="row">
+              <div className="col-12">
+                  <HomeSessionCount/>
+                  <HomeSessionFilter/>
+                  <HomeFilterView/>
+                  <HomeSessions sessions={props.sessions}
+                                updateSession={(rec) => {
+                                    return props.updateSession(rec);
+                                }}
+                  />
+              </div>
+          </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
+  
